de9159ff654ba7aa91239f6e70449a7d09779b4f
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-08 00:17:06 +0530

    vote for us

--- paradox.php
+++ paradox.php
@@ -111,6 +111,14 @@ if(isset($_POST['ans']))
 ?>
 
                 <div class="demo-card">
+                <div class="panel panel-danger">
+                            <div class="panel-heading">
+                                    <h3 class="panel-title"><?php echo $nam; ?>, Help us Grow </span></h3>
+                            </div>
+                            <div class="panel-body">
+                            If you find our projects/events (Paradox) interesting/worth suggesting please vote for us, It will help us grow. We are Team .EXE (.EXE), These voting lines will be open till 08 April, 2017 , 11:59PM <a href="http://technicaldeco.org/" target="_blank"><button class="btn btn-default" > Vote Here & help us grow</button></a>
+                            </div>
+
                         <div class="panel panel-info">
                             <div class="panel-heading">
                                     <h3 class="panel-title">Paradox Level #<?php echo $l; ?><span style="float: right"><?php echo $nam; ?></span>
@@ -122,11 +130,7 @@ if(isset($_POST['ans']))
                         echo "<img src=".$leve." />";   
                         echo ' <a href="leaderboard.php"><button class="btn btn-default" > View Paradox - Leaderboard </button></a>';
                         echo "<br><br>";
-                        echo ' <a href="hints.php"><button class="btn btn-default" > View Paradox - Hints </button></a>';
-                        echo "<br><br>";
-                        echo 'If you find our projects/events (Paradox) interesting/worth suggesting please vote for us, It will help us grow. We are Team .EXE (.EXE)<br>';
-                        echo ' <a href="http://technicaldeco.org/" target="_blank"><button class="btn btn-default" > Vote Here </button></a>';
-                                              
+                        echo ' <a href="hints.php"><button class="btn btn-default" > View Paradox - Hints </button></a>';                                              
                 ?>
                             </div>
                             <div class="panel-footer">
